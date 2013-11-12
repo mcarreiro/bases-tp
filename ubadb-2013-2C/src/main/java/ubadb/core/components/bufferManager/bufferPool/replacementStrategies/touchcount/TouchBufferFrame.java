64979222cebb76396b63100e8183cbf1a07480bb
martin301290@gmail.com <martin301290@gmail.com@9d4d6c80-eea0-f241-662d-7eb48ce26e0f>
2013-11-12 15:55:05 +0000

    Touch Buffer Frame

--- ubadb-2013-2C/src/main/java/ubadb/core/components/bufferManager/bufferPool/replacementStrategies/touchcount/TouchBufferFrame.java
+++ ubadb-2013-2C/src/main/java/ubadb/core/components/bufferManager/bufferPool/replacementStrategies/touchcount/TouchBufferFrame.java
@@ -1,15 +1,26 @@
 package ubadb.core.components.bufferManager.bufferPool.replacementStrategies.touchcount;
 
-import java.util.Date;
-
 import ubadb.core.components.bufferManager.bufferPool.BufferFrame;
 import ubadb.core.common.Page;
 import ubadb.core.exceptions.BufferFrameException;
 
 public class TouchBufferFrame extends BufferFrame {
         
+		public int count;
+	
         public TouchBufferFrame(Page page) {
                 super(page);
+                count = 0;
+        }
+        
+        public void pin(){
+        	super.pin();
+        	count++;
+        }
+        
+        public void unpin() throws BufferFrameException{
+        	super.unpin();
+        	count++;
         }
 
 }
