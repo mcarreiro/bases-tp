760b7bc2b4b9e777cccf0778e4fa5c6cee9a6962
martin301290@gmail.com <martin301290@gmail.com@9d4d6c80-eea0-f241-662d-7eb48ce26e0f>
2013-11-12 00:06:22 +0000

    LRU

--- ubadb-2013-2C/src/main/java/ubadb/core/components/bufferManager/bufferPool/replacementStrategies/lru/LRUBufferFrame.java
+++ ubadb-2013-2C/src/main/java/ubadb/core/components/bufferManager/bufferPool/replacementStrategies/lru/LRUBufferFrame.java
@@ -1,12 +1,33 @@
 package ubadb.core.components.bufferManager.bufferPool.replacementStrategies.lru;
 
+import java.util.Date;
+
 import ubadb.core.common.Page;
 import ubadb.core.components.bufferManager.bufferPool.BufferFrame;
+import ubadb.core.exceptions.BufferFrameException;
 
 public class LRUBufferFrame extends BufferFrame 
 {                
+		private Date referencedDate;
+	
         public LRUBufferFrame(Page page) {
-                super(page);            
+                super(page);     
+                referencedDate = new Date();
+        }
+        
+        public Date getReferencedDate()
+    	{
+    		return referencedDate;
+    	}
+        
+        public void pin(){
+        	super.pin();
+        	referencedDate = new Date();
+        }
+        
+        public void unpin() throws BufferFrameException{
+        	super.unpin();
+        	referencedDate = new Date();
         }
 
 
