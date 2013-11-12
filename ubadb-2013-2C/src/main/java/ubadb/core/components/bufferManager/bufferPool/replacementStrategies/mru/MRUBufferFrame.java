f8695640aada4a5d0cf492a3de1d3fc951fc2326
martin301290@gmail.com <martin301290@gmail.com@9d4d6c80-eea0-f241-662d-7eb48ce26e0f>
2013-11-12 00:01:33 +0000

    MRU

--- ubadb-2013-2C/src/main/java/ubadb/core/components/bufferManager/bufferPool/replacementStrategies/mru/MRUBufferFrame.java
+++ ubadb-2013-2C/src/main/java/ubadb/core/components/bufferManager/bufferPool/replacementStrategies/mru/MRUBufferFrame.java
@@ -1,12 +1,32 @@
 package ubadb.core.components.bufferManager.bufferPool.replacementStrategies.mru;
 import ubadb.core.common.Page;
 import ubadb.core.components.bufferManager.bufferPool.BufferFrame;
+import ubadb.core.exceptions.BufferFrameException;
+
+import java.util.Date;
 
 public class MRUBufferFrame extends BufferFrame 
 {
-                
+		private Date referencedDate;
+	
         public MRUBufferFrame(Page page) {
-                super(page);            
+                super(page); 
+                referencedDate = new Date();
+        }
+        
+        public Date getReferencedDate()
+    	{
+    		return referencedDate;
+    	}
+        
+        public void pin(){
+        	super.pin();
+        	referencedDate = new Date();
+        }
+        
+        public void unpin() throws BufferFrameException{
+        	super.unpin();
+        	referencedDate = new Date();
         }
 
 }
