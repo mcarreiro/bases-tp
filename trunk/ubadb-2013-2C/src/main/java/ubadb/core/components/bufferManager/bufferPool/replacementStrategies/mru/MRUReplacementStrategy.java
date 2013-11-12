b8e58716c4b7bb1f0f5ed95b675e459c4952019d
martin301290@gmail.com <martin301290@gmail.com@9d4d6c80-eea0-f241-662d-7eb48ce26e0f>
2013-11-12 00:01:33 +0000

    MRU

--- trunk/ubadb-2013-2C/src/main/java/ubadb/core/components/bufferManager/bufferPool/replacementStrategies/mru/MRUReplacementStrategy.java
+++ trunk/ubadb-2013-2C/src/main/java/ubadb/core/components/bufferManager/bufferPool/replacementStrategies/mru/MRUReplacementStrategy.java
@@ -1,6 +1,7 @@
 package ubadb.core.components.bufferManager.bufferPool.replacementStrategies.mru;
 
 import java.util.Collection;
+import java.util.Date;
 
 import ubadb.core.common.Page;
 import ubadb.core.components.bufferManager.bufferPool.BufferFrame;
@@ -11,16 +12,31 @@ public class MRUReplacementStrategy implements PageReplacementStrategy
 {       
         public BufferFrame findVictim(Collection<BufferFrame> bufferFrames) throws PageReplacementStrategyException
         {               
-
-        	return null;
+        	MRUBufferFrame victim = null;
+    		Date newestReplaceablePageDate = null;
+    		
+    		for(BufferFrame bufferFrame : bufferFrames)
+    		{
+    			MRUBufferFrame mruBufferFrame = (MRUBufferFrame) bufferFrame; //safe cast as we know all frames are of this type
+    			if(mruBufferFrame.canBeReplaced() && (newestReplaceablePageDate==null || mruBufferFrame.getReferencedDate().after(newestReplaceablePageDate)))
+    			{
+    				victim = mruBufferFrame;
+    				newestReplaceablePageDate = mruBufferFrame.getReferencedDate();
+    			}
+    		}
+    		
+    		if(victim == null)
+    			throw new PageReplacementStrategyException("No page can be removed from pool");
+    		else
+    			return victim;
         }
         
         public BufferFrame createNewFrame(Page page) 
         {
-                return null;                
+        	return new MRUBufferFrame(page);               
         }
         
         public String toString() {
-                return null;
+                return "MRU Replacement Strategy";
         }
 }
